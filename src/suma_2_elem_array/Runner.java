c885077ccb45fefebaa9b46b39300ba5b538f40b
cristicojo <crii_stii@yahoo.com>
2019-01-12 00:10:40 +0200

    new files

---
+++ src/suma_2_elem_array/Runner.java
@@ -0,0 +1,13 @@
+package suma_2_elem_array;
+
+public class Runner {
+    public static void main(String[] args) {
+        Suma2ElemArray s2=new Suma2ElemArray();
+
+        int a[]={1,2,3,4,5,6};
+
+        s2.suma(a);
+        s2.display();
+
+    }
+}
